7a58c2a4708e4caf7434f1a1aa3c4338ef014992
codineer <utkarshgamer294@outlook.com>
2024-10-30 23:00:52 +0530

    commit

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,13 +1,14 @@
 import { Account, Client, Databases } from "appwrite";
 
-export const PROJECT_ID = import.meta.env.PROJECT_ID
-export const DATABASE_ID = import.meta.env.DATABASE_ID
-export const COLLECTION_ID_MESSAGES = import.meta.env.COLLECTION_ID_MESSAGES
+export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID
+export const DATABASE_ID = import.meta.env.VITE_DATABASE_ID
+export const COLLECTION_ID_MESSAGES = import.meta.env.VITE_COLLECTION_ID_MESSAGES
 const client = new Client();
+// console.log(PROJECT_ID, DATABASE_ID, COLLECTION_ID_MESSAGES, import.meta.env)
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(import.meta.env.PROJECT_ID);
+    .setProject(import.meta.env.VITE_PROJECT_ID);
 
 export const databases = new Databases(client)
 export const account = new Account(client)
